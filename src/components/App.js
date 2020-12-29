4a2bb3b3afbf6abb7742461dab9161180c3c6b76
AshishSah10 <41590024+AshishSah10@users.noreply.github.com>
2020-12-29 13:54:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -33,10 +33,7 @@ class App extends Component {
     }
 }
 
-//export default App;
+export default App;
 module.exports = {
-    App: App,
-    Home: Home,
-    About: About,
     LocationDisplay: LocationDisplay
 }
